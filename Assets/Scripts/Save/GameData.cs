f43ecea3918f03eaba9330c4507aa5bc3bf886e1
ThAb <117899380+ThomasAbate@users.noreply.github.com>
2023-05-31 17:45:39 +0200

    fix

--- Assets/Scripts/Save/GameData.cs
+++ Assets/Scripts/Save/GameData.cs
@@ -8,9 +8,11 @@ public class GameData
     public int deathCount;
     public Vector3 PlayerPos;
     public ZerializableDictionary<string, bool> coinsCollected;
+    public float Volume;
     public GameData()
     {
         this.deathCount = 0;
+        this.Volume = 0;
         PlayerPos = Vector3.zero;
         coinsCollected = new ZerializableDictionary<string, bool>();
     }
--- Assets/Scripts/UI/Menu Paus.cs
+++ Assets/Scripts/UI/Menu Paus.cs
@@ -1,13 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuPaus : MonoBehaviour
 {
-    public GameObject menuPausPrefab;
+    public static bool gameIsPaused = false;
 
-    private void Update()
+    public GameObject pauseMenuUI;
+    void Update()
     {
-        if (KeyCode.P)
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+    }
+
+    void Paused()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void LoadMainMenu()
+    {
+        Resume();
+        SceneManager.LoadScene("Main Menu");
     }
 }
